b9b5aeb09bd9a106381511c8fddbe621a902f0e1
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-11-05 22:55:48 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,12 +1,18 @@
-class Post
-  
-  attr_accessor :title, :author
+class Song
+  attr_accessor :name, :artist
 
   @@all = []
 
-  def initialize(title)
-    @title = title
+  def initialize(name)
+    @name = name
     @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.name if artist
+  end
 end
